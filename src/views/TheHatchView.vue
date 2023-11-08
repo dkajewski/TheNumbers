9198021d96e4de4fab16e48188d6e47269eea52c
Damian Kajewski <orzechovaty@gmail.com>
2023-11-08 21:07:11 +0100

    added crt screen, version alpha

---
+++ src/views/TheHatchView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="monitor">
+        <div class="outer-frame">
+            <div class="inner-frame">
+                <div class="screen">
+                    > hello world
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+</script>
