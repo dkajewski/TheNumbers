9a1a6be2e8965234969b1587bb56fb92708e971f
Damian Kajewski <orzechovaty@gmail.com>
2024-03-18 19:23:55 +0100

    added working hieroglyphic clock after system failure activates

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,22 +1,22 @@
 <template>
     <div>
-        <div class="clock-frame">
+        <div class="clock-frame" v-show="!systemFailureActive">
             <div class="clock-container">
                 <div id="clock" class="tick">
-                    <span data-repeat="true" :class="{'system-failure': systemFailureActive}">
+                    <span data-repeat="true">
                         <span data-view="flip"></span>
                     </span>
                 </div>
                 <div id="seconds" class="tick">
-                    <span data-repeat="true" class="light-flip" :class="{'system-failure': systemFailureActive}">
+                    <span data-repeat="true" class="light-flip">
                         <span data-view="flip"></span>
                     </span>
                 </div>
             </div>
         </div>
-        <div class="clock-frame">
+        <div class="clock-frame" v-show="systemFailureActive">
             <div class="clock-container">
-                <div v-for="n in 5" :id="'hieroglyph' + n" class="tick">
+                <div v-for="n in 5" :id="'hieroglyph' + n" class="tick system-failure">
                     <span data-view="flip"></span>
                 </div>
             </div>
@@ -148,7 +148,7 @@ export default {
             // todo: move this somewhere else
             for (let i = 1; i <=5; i++) {
                 let element = document.getElementById('hieroglyph' + i);
-                this.mainClock = Tick.DOM.create(element, {
+                Tick.DOM.create(element, {
                     credits: false,
                     value: this.hieroglyphs[Math.floor(Math.random()*this.hieroglyphs.length)],
                     didInit: (tick) => {
